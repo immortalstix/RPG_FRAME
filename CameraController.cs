210766d4966d840cb9143d53fbe053ccf55a78be
immortalstix <rick.s@fanduel.com>
2017-06-30 11:42:36 -0400

    Add files via upload
    // script controllers as of #12 FRI 6/30/17

---
+++ CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject followTarget;
+	private Vector3 targetPos;
+	public float moveSpeed;
+
+	private static bool cameraExists;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (transform.gameObject);
+		if (!cameraExists) 
+		{
+			cameraExists = true;
+			DontDestroyOnLoad (transform.gameObject);
+		}  else {
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		targetPos = new Vector3 (followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+		// starting from, where we want to go to, how much movement we want (move speed * Time.deltaTime)
+		transform.position = Vector3.Lerp (transform.position, targetPos, moveSpeed * Time.deltaTime);
+	}
+}
---
+++ SlimeCont.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SlimeCont : MonoBehaviour {
+
+	public float moveSpeed;
+
+	private Rigidbody2D myRigidbody;
+
+	private bool moving; //Checks if the object is moving or not.
+
+	public float timeBetweenMove;
+	private float timeBetweenMoveCounter;
+
+	public float timeToMove;
+
+	private float timeToMoveCounter;
+
+	private Vector3 moveDirection;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+
+		//timeBetweenMoveCounter = timeBetweenMove;
+		//timeToMoveCounter = timeToMove;
+
+		timeBetweenMoveCounter = Random.Range (timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+		timeToMoveCounter = Random.Range (timeToMove * 0.75f, timeBetweenMove * 1.25f);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (moving)
+		{
+			timeToMoveCounter -= Time.deltaTime; // Ticks down the counter by Time.deltaTime.
+			myRigidbody.velocity = moveDirection;
+
+			if (timeToMoveCounter < 0f)
+			{
+				moving = false; // If no more time to move, movement is false.
+				//timeBetweenMoveCounter = timeBetweenMove;
+				timeBetweenMoveCounter = Random.Range (timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+			}
+
+		}
+
+		else
+		{
+			timeBetweenMoveCounter -= Time.deltaTime; // Ticks down the counter by Time.deltaTime.
+			myRigidbody.velocity = Vector2.zero; // If time has expired, reduces all velocity in all directions to zero.
+
+			if (timeBetweenMoveCounter < 0f)
+			{
+				moving = true;
+				//timeToMoveCounter = timeToMove; // Resetting moveTimeCounter to default value.
+				timeToMoveCounter = Random.Range (timeToMove * 0.75f, timeBetweenMove * 1.25f);
+				moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+
+
+			}
+		}
+	}
+}
