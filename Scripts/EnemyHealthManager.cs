d820ba6aaa19fca80b8947e3c1bcd68201293aab
Ricky Skawinski <rick.s@fanduel.com>
2017-10-26 16:25:20 -0400

    init commit of csharp scripts

---
+++ Scripts/EnemyHealthManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour {
+
+	public int MaxHealth;
+	public int CurrentHealth;
+
+
+	// Use this for initialization
+	void Start () {
+		CurrentHealth = MaxHealth;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (CurrentHealth <= 0) 
+		{
+			Destroy (gameObject);
+		}
+	}
+
+	public void HurtEnemy(int damageToGive)// int damage amount
+	{
+		CurrentHealth -= damageToGive; // '-=' is same as writing CurrentHealth = CurrentHealth - damageToGive
+	}
+
+	public void SetMaxHealth()
+	{
+		CurrentHealth = MaxHealth;
+	}
+}
---
+++ Scripts/HurtEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemy : MonoBehaviour {
+
+	public int damageToGive;
+	public GameObject damageBurst;
+	public Transform HitPoint;
+	public GameObject damageNumber;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Enemy")// "Enemy" is the tag of the object being selected
+		{
+			//Destroy(other.gameObject);
+			other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
+			Instantiate (damageBurst, HitPoint.position, HitPoint.rotation);
+			var clone = (GameObject) Instantiate(damageNumber, HitPoint.position, Quaternion.Euler (Vector3.zero)); 
+			clone.GetComponent<FloatingNumbers> ().damageNumber = damageToGive;
+		}
+		
+	}
+}
